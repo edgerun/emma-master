2b2c55fba22804c1b3c31bc92c2d31d6331d4adc
Thomas Rausch <thomas@rauschig.org>
2017-08-06 20:28:57 +0200

    Add UDP io utils to common module

---
+++ emma-common/src/main/java/at/ac/tuwien/dsg/emma/io/DatagramLoop.java
@@ -0,0 +1,52 @@
+package at.ac.tuwien.dsg.emma.io;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+
+import at.ac.tuwien.dsg.emma.util.IOUtils;
+
+/**
+ * DatagramLoop.
+ */
+public abstract class DatagramLoop implements Runnable, Closeable, ChannelHandler<DatagramChannel> {
+
+    private int port;
+
+    protected CommandLoop loop;
+    protected DatagramChannel channel;
+    protected SelectionKey key;
+
+    public DatagramLoop(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        try {
+            runChecked();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private void runChecked() throws IOException {
+        loop = new SimpleCommandLoop();
+
+        channel = DatagramChannel.open();
+        channel.configureBlocking(false);
+        channel.bind(new InetSocketAddress(port));
+
+        key = loop.register(channel, SelectionKey.OP_READ, this);
+
+        loop.run();
+    }
+
+    @Override
+    public void close() {
+        IOUtils.close(loop, channel);
+    }
+}
